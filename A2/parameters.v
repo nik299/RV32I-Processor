c48f8990689aa90547e8ebac0713a455550426ca
Nikhil Mattapally <45280030+nik299@users.noreply.github.com>
2019-09-19 08:12:11 +0530

    Add files via upload

---
+++ A2/parameters.v
@@ -0,0 +1,8 @@
+// Instruction / Data bus / Address width
+`define width 32
+//address parmeters used
+`define ADDRESS_WIDTH = 10;
+// Required for declaring the expected reg output array
+`define ADDRESS 1024
+//byte size
+`define BYTE =8
